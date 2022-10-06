9274f5d2b16611dab267ee25c610ed9a71f206c8
romadesign <roman247.rg@gmail.com>
2022-10-06 09:38:14 +0200

    loading en todas las pages

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,6 +1,42 @@
 import 'tailwindcss/tailwind.css'
+import '../../styles/globals.css'
+
+import {useRouter} from 'next/router'
+import {useState, useEffect} from 'react'
 
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />
+function Loading() {
+    const router = useRouter();
+
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const handleStart = (url) => (url !== router.asPath) && setLoading(true);
+        const handleComplete = (url) => (url === router.asPath) && setTimeout(() =>{setLoading(false)},4000);
+
+        router.events.on('routeChangeStart', handleStart)
+        router.events.on('routeChangeComplete', handleComplete)
+        router.events.on('routeChangeError',  handleComplete)
+
+        return () => {
+            router.events.off('routeChangeStart', handleStart)
+            router.events.off('routeChangeComplete', handleComplete)
+            router.events.off('routeChangeError', handleComplete)
+        }
+    })
+
+    return loading && (<div className='spinner-wrapper'>
+      <div className="spinner"></div></div>)
+  }
+
+
+// const App = ({ Component, pageProps }) => <Component {...pageProps} />
+function App ({ Component, pageProps }){
+    return (
+        <> <Loading />
+        <Component {...pageProps} />
+        </>
+    )
+}
 
 export default App
