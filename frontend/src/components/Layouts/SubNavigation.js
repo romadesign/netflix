1f4c86e3ee4f04f6ac54ed5b2a03d851b54dadaa
romadesign <roman247.rg@gmail.com>
2022-11-07 13:57:04 +0100

    create component modal alert

--- frontend/src/components/Layouts/SubNavigation.js
+++ frontend/src/components/Layouts/SubNavigation.js
@@ -6,135 +6,141 @@ import { useRouter } from 'next/router'
 import Link from 'next/link'
 
 const SubNavigation = ({ title }) => {
-    const router = useRouter()
-    const query = router.query
-    const genreGetUrl = query.type
+  const router = useRouter()
+  const query = router.query
+  const genreGetUrl = query.type
 
-    const { getCookie } = useAuth()
+  const { getCookie } = useAuth()
+  if (typeof window !== 'undefined') {
+    var name = getCookie('name')
+  }
+  const [genres, setGenres] = useState()
+  const [selectOptions, setSelectOptions] = useState(false)
+  const [selectOptionData, setSelectOptionData] = useState({ genreGetUrl: '' })
+
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true)
+      } else {
+        setIsScrolled(false)
+      }
+    }
     if (typeof window !== 'undefined') {
-        var name = getCookie('name')
-    }
-    const [genres, setGenres] = useState()
-    const [selectOptions, setSelectOptions] = useState(false)
-    const [selectOptionData, setSelectOptionData] = useState({ genreGetUrl: '' })
-
-    const [isScrolled, setIsScrolled] = useState(false)
-
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > 50) {
-                setIsScrolled(true)
-            } else {
-                setIsScrolled(false)
-            }
-        }
-        if (typeof window !== 'undefined') {
-            window.addEventListener('scroll', handleScroll)
-        }
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll)
-        }
-    }, [])
-
-    useEffect(() => {
-        getGenres()
-    }, [])
-
-    async function getGenres() {
-        const response = await axios.get(
-            `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/genres`,
-        )
-        const data = response.data.data
-        setGenres(data)
+      window.addEventListener('scroll', handleScroll)
     }
 
-    const handleChange = e => {
-        setSelectOptions(true)
-        if (selectOptions == !true) {
-            setSelectOptions(!false)
-        } else {
-            setSelectOptions(!true)
-        }
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
     }
+  }, [])
 
-    const onMouseLeave = () => {
-        setSelectOptions(false)
-    }
+  useEffect(() => {
+    getGenres()
+  }, [])
 
-    const urlSeries = router.pathname === '/series' || router.pathname === '/series/genre/[id]'
-    const urlFilms = router.pathname === '/peliculas' || router.pathname === '/peliculas/genre/[id]'
-
-    return (
-        <>
-            <div
-                className={`${router.pathname === '/films'
-                    ? styles.subnavbar
-                    : styles.subnavbarFilms
-                    }`}>
-                <div
-                    className={`${!isScrolled ? styles.subnavbar_text : styles.subnavbarContentGenre
-                        }`}>
-                    {title}
-                    <div className={styles.content_navbar}>
-                        <div className={styles.select_options} onClick={handleChange}>
-                            {selectOptionData.genreGetUrl == '' ? (
-                                <>
-                                    {genreGetUrl == undefined ? (
-                                        <>Seleccionar</>
-                                    ) : (
-                                        <>{genreGetUrl}</>
-                                    )}
-                                </>
-                            ) : (
-                                <>{selectOptionData.genre}</>
-                            )}
-                        </div>
-                        {selectOptions && (
-                            <div
-                                className={`${!isScrolled
-                                    ? styles.content_sub_navbar
-                                    : styles.content_navbarOther
-                                    }`}
-                                onMouseLeave={onMouseLeave}>
-                                <>
-                                    {genres !== undefined &&
-                                        genres.map(genre => (
-                                            <div
-                                                className={styles.urlId}
-                                                onClick={() =>
-                                                    setSelectOptionData({
-                                                        genre: genre.title,
-                                                    })
-                                                }>
-                                                {urlSeries &&
-                                                    <Link
-                                                        href={{
-                                                            pathname: `/series/genre/${genre.id}`,
-                                                            query: { type: genre.title }, // the data genre,
-                                                        }}>
-                                                        {genre.title}
-                                                    </Link>
-                                                }
-                                                {urlFilms &&
-                                                    <Link
-                                                        href={{
-                                                            pathname: `/peliculas/genre/${genre.id}`,
-                                                            query: { type: genre.title }, // the data genre,
-                                                        }}>
-                                                        {genre.title}
-                                                    </Link>
-                                                }
-                                            </div>
-                                        ))}
-                                </>
-                            </div>
-                        )}
-                    </div>
-                </div>
-            </div>
-        </>
+  async function getGenres () {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/genres`,
     )
+    const data = response.data.data
+    setGenres(data)
+  }
+
+  const handleChange = e => {
+    setSelectOptions(true)
+    if (selectOptions == !true) {
+      setSelectOptions(!false)
+    } else {
+      setSelectOptions(!true)
+    }
+  }
+
+  const onMouseLeave = () => {
+    setSelectOptions(false)
+  }
+
+  const urlSeries =
+    router.pathname === '/series' || router.pathname === '/series/genre/[id]'
+  const urlFilms =
+    router.pathname === '/peliculas' ||
+    router.pathname === '/peliculas/genre/[id]'
+
+  return (
+    <>
+      <div
+        className={`${
+          router.pathname === '/films'
+            ? styles.subnavbar
+            : styles.subnavbarFilms
+        }`}>
+        <div
+          className={`${
+            !isScrolled ? styles.subnavbar_text : styles.subnavbarContentGenre
+          }`}>
+          {title}
+          <div className={styles.content_navbar}>
+            <div className={styles.select_options} onClick={handleChange}>
+              {selectOptionData.genreGetUrl == '' ? (
+                <>
+                  {genreGetUrl == undefined ? (
+                    <>Seleccionar</>
+                  ) : (
+                    <>{genreGetUrl}</>
+                  )}
+                </>
+              ) : (
+                <>{selectOptionData.genre}</>
+              )}
+            </div>
+            {selectOptions && (
+              <div
+                className={`${
+                  !isScrolled
+                    ? styles.content_sub_navbar
+                    : styles.content_navbarOther
+                }`}
+                onMouseLeave={onMouseLeave}>
+                <>
+                  {genres !== undefined &&
+                    genres.map(genre => (
+                      <div
+                        className={styles.urlId}
+                        onClick={() =>
+                          setSelectOptionData({
+                            genre: genre.title,
+                          })
+                        }>
+                        {urlSeries && (
+                          <Link
+                            href={{
+                              pathname: `/series/genre/${genre.id}`,
+                              query: { type: genre.title }, // the data genre,
+                            }}>
+                            {genre.title}
+                          </Link>
+                        )}
+                        {urlFilms && (
+                          <Link
+                            href={{
+                              pathname: `/peliculas/genre/${genre.id}`,
+                              query: { type: genre.title }, // the data genre,
+                            }}>
+                            {genre.title}
+                          </Link>
+                        )}
+                      </div>
+                    ))}
+                </>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default SubNavigation
